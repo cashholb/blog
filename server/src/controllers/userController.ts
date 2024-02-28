7913be3d4750e635cca78146db867012651c5e21
cashtonholbert <cashtoncoleholbert@gmail.com>
2024-02-28 00:09:59 -0800

    user controller with all CRUD operations

---
+++ server/src/controllers/userController.ts
@@ -0,0 +1,126 @@
+import { Request, Response } from 'express';
+import asyncHandler from "express-async-handler";
+import { body, validationResult } from "express-validator";
+
+import Comment from '../models/comment';
+import Post  from '../models/post';
+import User from '../models/user'
+import { Types } from 'mongoose';
+import comment from '../models/comment';
+
+// -- Create --
+export const createUser = [
+  body("username")
+    .trim()
+    .isLength({ min: 3, max: 20 })
+    .withMessage("Username cannot contain trailing/leading whitespace and must be withing 3 to 20 characters")
+    .escape(),
+  body('email')
+    .isEmail(),
+  body("password")
+    .trim()
+    .isLength({ min: 3, max: 100 })
+    .withMessage("Password cannot contain trailing/leading whitespace and must be withing 3 to 100 characters")
+    .escape(),
+
+  asyncHandler( async (req: Request, res: Response) => {
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    const { username, email, password } = req.body;
+    
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: password
+    });
+
+    await newUser.save();
+
+    res.json(newUser);
+  }),
+];
+
+// -- Read --
+export const getAllUsers = asyncHandler( async (req: Request, res: Response) => {
+  const users = await User.find();
+  if(!users) {
+    res.status(404).json({ message: 'No users found' });
+  }
+  res.json(users);
+});
+
+export const getUser = asyncHandler( async (req: Request, res: Response) => {
+  const user = await User.findById(req.params.userId);
+  if(!user) {
+    res.status(404).json({ message: 'User not found' });
+  }
+  res.json(user);
+});
+
+// -- Update --
+export const updateUser = [
+  body("username")
+    .optional({nullable: true})
+    .trim()
+    .isLength({ min: 3, max: 20 })
+    .withMessage("Username cannot contain trailing/leading whitespace and must be withing 3 to 20 characters")
+    .escape(),
+  body('email')
+    .optional({nullable: true})
+    .isEmail(),
+  body("password")
+    .optional({nullable: true})
+    .trim()
+    .isLength({ min: 3, max: 100 })
+    .withMessage("Password cannot contain trailing/leading whitespace and must be withing 3 to 100 characters")
+    .escape(),
+
+  asyncHandler( async (req: Request, res: Response) => {
+
+    // Check for validation errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(400).json({ errors: errors.array() });
+    }
+
+    const userToUpdate = await User.findById(req.params.userId);
+    if(!userToUpdate) {
+      res.status(404).json({ message: 'User not found' });
+    }
+
+    // update fields if specified
+    const { username, email, password } = req.body;
+    if(username) userToUpdate.username = username;
+    if(email) userToUpdate.email = email;
+    if(password) userToUpdate.password = password;
+
+    await userToUpdate.save();
+
+    res.json(userToUpdate);
+  }),
+];
+
+// -- Delete --
+export const deleteUser = asyncHandler( async (req: Request, res: Response) => {
+  
+  // check if user has any posts and/or comments and deny the deletion if so
+  const [ postsByUser, commentsByUser ] = await Promise.all([
+    Post.find({author: req.params.userId}),
+    Comment.find({user: req.params.userId})
+  ]);
+  if ((commentsByUser.length > 0) || (postsByUser.length > 0)) {
+    console.log(typeof commentsByUser);
+    console.log(postsByUser);
+    res.status(403).json({ message: `Forbidden: must first delete all ${commentsByUser && postsByUser ? 'comments and posts' : (commentsByUser ? 'comments' : 'posts')} by user` })
+  }
+
+  const deletedUser = await User.findByIdAndDelete(req.params.userId)
+  if(!deletedUser) {
+    res.status(404).json({ message: 'User not found' });
+  }
+  res.json(deletedUser);
+});
--- server/src/routes/blogRouter.ts
+++ server/src/routes/blogRouter.ts
@@ -3,13 +3,14 @@ const router: Router = express.Router();
 
 import { getAllPosts, getPost, createPost, deletePost, updatePost } from '../controllers/postController';
 import { createComment, getAllCommentsOfPost, getComment, updateComment, deleteComment } from '../controllers/commentController'
+import { createUser, getAllUsers, getUser, updateUser, deleteUser } from '../controllers/userController'
 
 // Posts
 router.post('/posts/:userId', createPost);
 router.get('/posts', getAllPosts);
 router.get('/posts/:postId', getPost);
 router.put('/posts/:postId', updatePost);
-router.delete('/posts/delete/:userId', deletePost);
+router.delete('/posts/:postId', deletePost);
 
 // Comments
 router.post('/posts/:postId/comments/:userId', createComment);
@@ -21,5 +22,10 @@ router.put('comments/:commentId', updateComment);
 router.delete('/posts/:postId/comments/:commentId', deleteComment);
 
 // Users
+router.post('/users', createUser);
+router.get('/users', getAllUsers);
+router.get('/users/:userId', getUser);
+router.put('/users/:userId', updateUser);
+router.delete('/users/:userId', deleteUser);
 
 export default router
